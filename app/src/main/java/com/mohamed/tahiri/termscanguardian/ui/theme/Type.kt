8d0cd977caf905187fd366a9adc4fdb8f948c74d
mohamed-tahiri <medtohairi@gmail.com>
2024-03-11 00:15:20 +0000

    add font

--- app/src/main/java/com/mohamed/tahiri/termscanguardian/ui/theme/Type.kt
+++ app/src/main/java/com/mohamed/tahiri/termscanguardian/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.mohamed.tahiri.termscanguardian.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mohamed.tahiri.termscanguardian.R
+
+val my_font = FontFamily(
+    Font(R.font.opensans_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,15 +20,15 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = my_font,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    /* Other default text styles to override
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
